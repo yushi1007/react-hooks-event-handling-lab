a326d08c8bb11cf58543d4aec681d04c9e93dcc4
Yu Shi <yushiys95@gmail.com>
2021-04-06 19:50:47 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyeOnMe() {
+  function handleFocus(event) {
+    console.log("Good!");
+  }
+  function handleBlur(event) {
+    console.log("Hey! Eyes on me!");
+  }
+  return (
+      <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+  );
+}
+
+export default EyeOnMe;
