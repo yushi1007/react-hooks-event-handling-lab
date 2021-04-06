36c84c3b50c4f3846447bf98a6f14b65f0b61957
Yu Shi <yushiys95@gmail.com>
2021-04-06 19:50:42 -0400

    commit tree 9b921c32c116e7392547586e51a0dc6c96801c25

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,22 @@
 // Code Keypad Component Here
+import React from "react";
+// Keypad React component
+function Keypad() {
+    // add an event handler that listens for the change event
+    function handleChange(event) {
+        // When that event fires, use console.log to print out the text 'Entering password...'.
+      console.log('Entering password...');
+    }
+  
+    return (
+        // render an input with the right type
+        <input
+        type="password"
+        // On that input, add an event handler that listens for the change event.
+        onChange={handleChange}
+      />
+    );
+  }
+  
+  export default Keypad;
+  
